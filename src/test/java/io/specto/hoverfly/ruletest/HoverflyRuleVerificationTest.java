54d6329100130c54ba96e4848d758623f11c7ff6
Tommy Situ <tommy.situ@specto.io>
2020-11-02 21:49:05 +0000

    #241 Extends AssertionError for verification to work with Awaitility polling

--- src/test/java/io/specto/hoverfly/ruletest/HoverflyRuleVerificationTest.java
+++ src/test/java/io/specto/hoverfly/ruletest/HoverflyRuleVerificationTest.java
@@ -1,10 +1,33 @@
 package io.specto.hoverfly.ruletest;
 
+import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
+import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
+import static io.specto.hoverfly.junit.dsl.HttpBodyConverter.json;
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.any;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.contains;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.equalsToJson;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.matches;
+import static io.specto.hoverfly.junit.verification.HoverflyVerifications.never;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.specto.hoverfly.junit.dsl.HttpBodyConverter;
 import io.specto.hoverfly.junit.rule.HoverflyRule;
 import io.specto.hoverfly.junit.verification.HoverflyVerificationError;
 import io.specto.hoverfly.models.SimpleBooking;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+import java.time.LocalDate;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -14,26 +37,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.time.LocalDate;
-
-import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
-import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
-import static io.specto.hoverfly.junit.dsl.HttpBodyConverter.json;
-import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
-import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.*;
-import static io.specto.hoverfly.junit.verification.HoverflyVerifications.never;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 public class HoverflyRuleVerificationTest {
 
     private static final String NL = System.lineSeparator();
-    private RestTemplate restTemplate = new RestTemplate();
+    private static final SimpleBooking BOOKING = new SimpleBooking(1, "London", "Hong Kong", LocalDate.of(2017, 6, 29));
+    private final RestTemplate restTemplate = new RestTemplate();
 
-    private static SimpleBooking booking = new SimpleBooking(1, "London", "Hong Kong", LocalDate.of(2017, 6, 29));
 
     @ClassRule
     public static HoverflyRule hoverflyRule = HoverflyRule.inSimulationMode(dsl(
@@ -42,10 +51,10 @@ public class HoverflyRuleVerificationTest {
                     .get("/api/bookings")
                     .queryParam("airline", contains("Pacific"))
                     .queryParam("page", any())
-                    .willReturn(success(json(booking)))
+                    .willReturn(success(json(BOOKING)))
 
                     .put("/api/bookings/1")
-                    .body(equalsToJson(json(booking)))
+                    .body(equalsToJson(json(BOOKING)))
                     .willReturn(success())
 
     )).printSimulationData();
@@ -84,7 +93,7 @@ public class HoverflyRuleVerificationTest {
 
         assertThat(bookFlightResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        hoverflyRule.verify(service("http://api-sandbox.flight.com").put("/api/bookings/1").header("Content-Type", any()).body(json(booking)));
+        hoverflyRule.verify(service("http://api-sandbox.flight.com").put("/api/bookings/1").header("Content-Type", any()).body(json(BOOKING)));
     }
 
     @Test
@@ -144,6 +153,32 @@ public class HoverflyRuleVerificationTest {
                        "But actual number of requests is 0.");
     }
 
+    @Test
+    public void shouldBeCompatibleWithAwaitility() {
+        delayedRequest();
+
+        await().pollInterval(Duration.ofMillis(100))
+            .untilAsserted(() -> hoverflyRule.verify(service("service")
+                    .post("/path")
+                    .anyBody()));
+    }
+
+    private void delayedRequest() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+                String host = String.format("localhost:%s", hoverflyRule.getProxyPort());
+                HttpHost proxy = HttpHost.create(host);
+                HttpClientBuilder.create()
+                    .setProxy(proxy)
+                    .build()
+                    .execute(new HttpPost("http://service/path"));
+            } catch (IOException | InterruptedException e) {
+                Assert.fail("Fail to make a delayed request: " + e.getMessage());
+            }
+        }).start();
+    }
+
     private ResponseEntity<SimpleBooking> getBookings() {
         URI uri = UriComponentsBuilder.fromHttpUrl("http://api-sandbox.flight.com")
                 .path("/api/bookings")
@@ -160,7 +195,7 @@ public class HoverflyRuleVerificationTest {
     private ResponseEntity<String> putBooking() throws URISyntaxException, JsonProcessingException {
         RequestEntity<String> bookFlightRequest = RequestEntity.put(new URI("http://api-sandbox.flight.com/api/bookings/1"))
                 .contentType(APPLICATION_JSON)
-                .body(HttpBodyConverter.OBJECT_MAPPER.writeValueAsString(booking));
+                .body(HttpBodyConverter.OBJECT_MAPPER.writeValueAsString(BOOKING));
 
         return restTemplate.exchange(bookFlightRequest, String.class);
     }
