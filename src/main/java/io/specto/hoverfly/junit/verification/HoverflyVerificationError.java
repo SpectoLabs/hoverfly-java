7fa5fb13b07ef98c5358677ff24048f94f79a508
Tommy Situ <tommy.situ@specto.io>
2020-11-02 21:49:05 +0000

    #241 Extends AssertionError for verification to work with Awaitility polling

--- src/main/java/io/specto/hoverfly/junit/verification/HoverflyVerificationError.java
+++ src/main/java/io/specto/hoverfly/junit/verification/HoverflyVerificationError.java
@@ -1,6 +1,6 @@
 package io.specto.hoverfly.junit.verification;
 
-public class HoverflyVerificationError extends Error {
+public class HoverflyVerificationError extends AssertionError {
 
     public HoverflyVerificationError(String message) {
         super(message);
